8df71297bf987d158656258da08ca8ecf01037d4
pablo <stevaopabllo@gmail.com>
2025-02-14 12:57:21 -0300

    esboço

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const cars = {
+    "porsche_gt3_rs": "https://sketchfab.com/models/bf6aaaad5093428da6713db916045963/embed?autospin=1&autostart=1&preload=1&transparent=1&ui_theme=dark",
+    "mercedes_amg_gt_63": "https://sketchfab.com/3d-models/mercedes-amg-gt-63-572e320f4e054f919d316afb2676d292?utm_medium=embed&utm_campaign=share-popup&utm_content=572e320f4e054f919d316afb2676d292"
+}
